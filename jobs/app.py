2a21538f8f73890be019749710e68dd85423e48c
Sasi-R <sasikumarirtt20@gmail.com>
2019-04-24 01:14:41 +0530

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -9,8 +9,8 @@ def open_connection():
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
-def execute_sql(sql, values(), commit=False, single= False):
-    connection = open.connection()
+def execute_sql(sql, values=(), commit=False, single= False):
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
